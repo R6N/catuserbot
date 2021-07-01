5e3cf6ab35b807a7f18918e9d23e201fcea242ef
R6N <69646277+R6N@users.noreply.github.com>
2021-07-01 13:33:15 +0300

    Update filters.py

--- userbot/plugins/filters.py
+++ userbot/plugins/filters.py
@@ -28,13 +28,39 @@ async def filter_incoming_handler(handler):
             for trigger in filters:
                 pattern = r"( |^|[^\w])" + re.escape(trigger.keyword) + r"( |$|[^\w])"
                 if re.search(pattern, name, flags=re.IGNORECASE):
+                    count = len(participants)
+                    mention = "<a href='tg://user?id={}'>{}</a>".format(a_user.id, a_user.first_name)
+                    my_mention = "<a href='tg://user?id={}'>{}</a>".format(me.id, me.first_name)
+                    first = a_user.first_name
+                    last = a_user.last_name
+                    fullname = f"{first} {last}" if last else first
+                    username = f"@{a_user.username}" if a_user.username else mention
+                    userid = a_user.id
+                    my_first = me.first_name
+                    my_last = me.last_name
+                    my_fullname = f"{my_first} {my_last}" if my_last else my_first
+                    my_username = f"@{me.username}" if me.username else my_mention
                     if trigger.f_mesg_id:
                         msg_o = await handler.client.get_messages(
                             entity=BOTLOG_CHATID, ids=int(trigger.f_mesg_id)
                         )
                         await handler.reply(msg_o.message, file=msg_o.media)
                     elif trigger.reply:
-                        await handler.reply(trigger.reply)
+                        await handler.reply(str(trigger.reply).format(
+                            mention=mention,
+                            title=title,
+                            count=count,
+                            first=first,
+                            last=last,
+                            fullname=fullname,
+                            username=username,
+                            userid=userid,
+                            my_first=my_first,
+                            my_last=my_last,
+                            my_fullname=my_fullname,
+                            my_username=my_username,
+                            my_mention=my_mention,
+                        ))
     except AttributeError:
         pass
 
